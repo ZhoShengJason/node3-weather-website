5b5d599683abb468c3f41d6e8719254774b82d45
Zhongyang Sheng -X (zhosheng - ZENSAR TECHNOLOGIES INC at Cisco) <zhosheng@cisco.com>
2020-04-29 13:33:32 -0700

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
-
+const port = process.env.PORT || 3000
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -98,6 +98,7 @@ app.get('*',(req,res)=>{
 //app.com
 //app.com/help
 
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000')
+
+app.listen(port, ()=>{
+    console.log('Server is up on port ' + port)
 })
